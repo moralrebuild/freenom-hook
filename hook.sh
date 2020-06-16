d06fa089c105f04348b3b7109ff50c8d8f9d6917
Ivan Vatlin <jenrus@riseup.net>
2020-06-17 00:20:20 +0300

    change email here too

--- hook.sh
+++ hook.sh
@@ -2,7 +2,7 @@
 
 # DNS-01 challenge hook for Freenom used in dehydrated, a Let's Encrypt client
 
-# Copyright © 2020 Ivan Vatlin <jenrus@tuta.io>
+# Copyright © 2020 Ivan Vatlin <jenrus@riseup.net>
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
